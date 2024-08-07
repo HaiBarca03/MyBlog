97fdc65765e74aabc6c3598e41b596c3e7cd2228
HaiBarca03 <doanduchai8d2m@gmail.com>
2024-08-07 21:41:15 +0700

    ok

--- server.js
+++ server.js
@@ -10,13 +10,11 @@ const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 const DATABASE_HOST = process.env.DATABASE_HOST;
 const DATABASE_NAME = process.env.DATABASE_NAME;
 const DATABASE = `mongodb+srv://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}/${DATABASE_NAME}?retryWrites=true&w=majority&appName=MyPersonalBlog`;
-const DATABASE_LOCAL = process.env.DATABASE_LOCAL
 
 // connect db
-mongoose.connect(DATABASE) 
-// mongoose.connect(DATABASE_LOCAL)
-.then(() => console.log('Database connected successfully'))
-.catch((error) => {console.log(error)})
+mongoose.connect(DATABASE)
+    .then(() => console.log('Database connected successfully'))
+    .catch((error) => { console.log(error) })
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`)
